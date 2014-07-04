66118f589524b076056f10b3c56cf24de2ffa830
fieldlinguist <info@fieldlinguist.com>
2014-07-04 10:58:57 -0400

    for #1469 and #1394 renamed lexiconservice to be more specific

--- switch_to_develop_easy_setup.sh
+++ switch_to_develop_easy_setup.sh
@@ -2,10 +2,10 @@
 
 echo ""
 echo "Put the Node server to act as the development server"
-sed 's/nodeconfig_[^)]*)/nodeconfig_devserver")/' service.js  > output
-mv output service.js
-sed 's/couchkeys_[^)]*)/couchkeys_devserver")/' service.js  > output
-mv output service.js
+sed 's/nodeconfig_[^)]*)/nodeconfig_devserver")/' lexicon-service.js  > output
+mv output lexicon-service.js
+sed 's/couchkeys_[^)]*)/couchkeys_devserver")/' lexicon-service.js  > output
+mv output lexicon-service.js
 
 echo "Now running in developer mode."
 
--- switch_to_develop_locally_offline.sh
+++ switch_to_develop_locally_offline.sh
@@ -2,10 +2,10 @@
 
 echo ""
 echo "Put the Node server to act as the localhost server"
-sed 's/nodeconfig_[^)]*)/nodeconfig_local")/' service.js  > output
-mv output service.js
-sed 's/couchkeys_[^)]*)/couchkeys_local")/' service.js  > output
-mv output service.js
+sed 's/nodeconfig_[^)]*)/nodeconfig_local")/' lexicon-service.js  > output
+mv output lexicon-service.js
+sed 's/couchkeys_[^)]*)/couchkeys_local")/' lexicon-service.js  > output
+mv output lexicon-service.js
 
 echo "Now running in local/offline developer mode using the localhost."
 
